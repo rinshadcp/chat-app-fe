74dbf53af7be77f217fda8f8be96bea330374673
rinshadcp <rinshadcp7337@gmail.com>
2024-09-28 19:25:00 +0530

    setup vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl font-bold text-center m-5  text-red-700">
+        Hi this is starting !!!
+      </div>
+    </>
+  );
+}
+
+export default App;
