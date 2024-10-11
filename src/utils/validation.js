c1143d57bf8364f844289935a7c5cd58a1daf22b
rinshadcp <rinshadcp7337@gmail.com>
2024-10-12 04:33:38 +0530

    updated

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,6 @@
 export const validateSigninData = (fullName, email, password) => {
   const emailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email);
-  const passwordValid =
-    /^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+  const passwordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
   const isFullNameValid =
     fullName !== null
       ? /^[A-Za-z]{3,16}( [A-Za-z]{3,16}){0,3}$/.test(fullName)
